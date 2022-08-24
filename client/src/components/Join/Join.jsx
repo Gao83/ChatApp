2aca85ffb7b6aa2f26f2f7c76eca1069b4b8d5b7
Isaac Miralles <isaacmiralles@gmail.com>
2022-08-24 12:23:13 +0200

    server: socket io connect/disconnect &  client: Join component done with styiling

--- client/src/components/Join/Join.jsx
+++ client/src/components/Join/Join.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Join.css'
 
 function Join() {
+
+  const [name, setName] = useState('')
+  const [room, setRoom] = useState('')
+
+  const handleName = (event) => {
+    setName(event.target.value)
+    console.log(event.target.value)
+  }
+
+  const handleRoom = (event) => {
+    setRoom(event.target.value)
+  }
+
+  const handleClick = (event) => (
+    (!room || !name) ? event.preventDefault(alert('You need to fill the fields')) : null
+  )
+
   return (
-    <div>Join</div>
+    <div className="join__outter-container">
+      <div className="join__inner-container">
+        <h1 className="heading">Join</h1>
+        <div><input type="text" placeholder='Introduce your name' className='join__input' onChange={handleName} /></div>
+        <div><input type="text" placeholder='Select the chat room' className='join__input' onChange={handleRoom} /></div>
+        <Link to={`/chat?name=${name}&${room}`}>
+          <button className='btn mt-20' type='submit' onClick={handleClick}> Sing In</button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+io.on('conection', (socket) => {
+console.log('we have a new conexion!!!!')
+
+socket.on('disconnect', ()=>{
+    console.log('user have left!!')
+})
+})
+
 app.use(router)
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`))
